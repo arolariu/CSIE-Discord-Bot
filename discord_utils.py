1a9ad08b5e48e5413fc7a61e42bc98bd1e9ad874
arolariu <olariu.alexandru@protonmail.ch>
2021-02-22 18:03:33 +0200

    Version 2.3b

--- discord_utils.py
+++ discord_utils.py
@@ -8,7 +8,7 @@ LOGO_GIF = "https://cdn.discordapp.com/attachments/743466478707409037/7449201744
 ##################
 
 VERSION_DATE = "21/02/2021"
-VERSION_NUMBER = "2.2f"
+VERSION_NUMBER = "2.3b"
 VERSION_DATA = \
     """
 ++ Toate librariile interne actualizate la ultima versiune.
